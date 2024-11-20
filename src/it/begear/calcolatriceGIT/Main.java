273c9e9233dbcd02a2b9bd7d7bdde643e48b1240
michele <michele.perillo16@gmail.com>
2024-11-20 09:42:59 +0100

    Aggiunta Divisione by Michele Perillo

--- src/it/begear/calcolatriceGIT/Main.java
+++ src/it/begear/calcolatriceGIT/Main.java
@@ -6,11 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Calcolatrice GIT");
-
-        int a = 1;
+        int a = 10;
         int b = 2;
 
         System.out.println("Somma: " + Calcoli.somma(a,b));
-
+        System.out.println("Divisione: " + Calcoli.divisione(a,b));
     }
 }
--- src/it/ebegar/calcolatriceGIT/utility/Calcoli.java
+++ src/it/ebegar/calcolatriceGIT/utility/Calcoli.java
@@ -6,5 +6,15 @@ public class Calcoli {
     public static int somma (int a, int b) {
         return a+b;
     }
+    // Michele Perillo aggiunta metodo divisione
+    public static String divisione (int a, int b){
+        String res;
 
+        try {
+            res = a / b +"";
+        }catch(ArithmeticException e){
+            res = "non è possibile dividere un numero per 0";
+        }
+        return res;
+    }
 }
