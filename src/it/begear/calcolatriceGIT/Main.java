22e1486ee2732d1ca5fc584e02580940e91d8f4d
PMateria <pietromateria1193@gmail.com>
2024-11-20 09:43:12 +0100

    [Pietro Materia V0.1] Sottrazione tra 3 numeri

--- src/it/begear/calcolatriceGIT/Main.java
+++ src/it/begear/calcolatriceGIT/Main.java
@@ -10,7 +10,13 @@ public class Main {
         int a = 1;
         int b = 2;
 
+        // Pietro Materia 20/11/2024 v0.1
+        int c= 3;
+
         System.out.println("Somma: " + Calcoli.somma(a,b));
+
+        // Pietro Materia 20/11/2024 v0.1
+        System.out.println("Sottrazione " + Calcoli.sottrazione(a,b,c));
 
     }
 }
